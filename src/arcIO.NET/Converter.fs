00f17b695f7d394b03e80e6de077a8ff44345b52
Heinrich Lukas Weil <hl.weil@t-online.de>
2023-02-23 16:59:45 +0100

    add converter type

---
+++ src/arcIO.NET/Converter.fs
@@ -0,0 +1,14 @@
+﻿namespace arcIO.NET.Converter
+
+open ISADotNet
+open ISADotNet.QueryModel
+open FsSpreadsheet.DSL
+open LitXml
+
+
+type ARCconverter =
+| ARCtoCSV      of (QInvestigation -> QStudy -> QAssay -> SheetEntity<Workbook>)
+| ARCtoTSV      of (QInvestigation -> QStudy -> QAssay -> SheetEntity<Workbook>)
+| ARCtoXLSX     of (QInvestigation -> QStudy -> QAssay -> SheetEntity<Workbook>)
+| ARCtoXML      of (QInvestigation -> QStudy -> QAssay -> LitXml.XmlPart)
+//| ARCtoJSON     of QInvestigation -> QStudy -> QAssay -> 
