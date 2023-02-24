e42fd9dabcc325b1c888995fa11563ec426679f2
Heinrich Lukas Weil <hl.weil@t-online.de>
2023-02-24 16:15:32 +0100

    add converter helper function

--- src/arcIO.NET/Converter.fs
+++ src/arcIO.NET/Converter.fs
@@ -12,3 +12,25 @@ type ARCconverter =
 | ARCtoXLSX     of (QInvestigation -> QStudy -> QAssay -> SheetEntity<Workbook>)
 | ARCtoXML      of (QInvestigation -> QStudy -> QAssay -> LitXml.XmlPart)
 //| ARCtoJSON     of QInvestigation -> QStudy -> QAssay -> 
+
+    member this.ConvertCSV(i,s,a) = 
+        match this with
+        | ARCtoCSV f -> f i s a
+        | _ -> failwith "could not convert to csv"
+
+    member this.ConvertTSV(i,s,a) = 
+        match this with
+        | ARCtoTSV f -> f i s a
+        | _ -> failwith "could not convert to tsv"
+
+    member this.ConvertXLSX(i,s,a) = 
+        match this with
+        | ARCtoXLSX f -> f i s a
+        | _ -> failwith "could not convert to xlsx"
+
+    member this.ConvertXML(i,s,a) = 
+        match this with
+        | ARCtoXML f -> f i s a
+        | _ -> failwith "could not convert to xml"
+
+
