47011f8d30f3d72d927f4540a9515e55c0028f22
Heinrich Lukas Weil <hl.weil@t-online.de>
2023-09-07 14:45:49 +0200

    add more contraints to getAllpaths fix #10

--- src/ARCtrl.NET/Path.fs
+++ src/ARCtrl.NET/Path.fs
@@ -6,11 +6,22 @@ let ensureDirectory (filePath : string) =
     let file = new System.IO.FileInfo(filePath);
     file.Directory.Create()
 
+/// Return the absolute path relative to the directoryPath
+let makeRelative directoryPath (path : string) = 
+    if directoryPath = "." || directoryPath = "/" || directoryPath = "" then path
+    else
+        if path.StartsWith(directoryPath) then 
+            path.Substring(directoryPath.Length)
+        else path
+
+let standardizeSlashes (path : string) = 
+    path.Replace("\\","/")              
+
 let getAllFilePaths (directoryPath : string) =
     let rec allFiles dirs =
         if Seq.isEmpty dirs then Seq.empty else
             seq { yield! dirs |> Seq.collect Directory.EnumerateFiles
                   yield! dirs |> Seq.collect Directory.EnumerateDirectories |> allFiles }
-
+    
     allFiles [directoryPath] |> Seq.toArray
-    |> Array.map (fun p -> p.Replace(directoryPath, "").Replace("\\","/"))
+    |> Array.map (makeRelative directoryPath >> standardizeSlashes)
