d82fdf6f434902341e1da4dc26fc92da5c288fe6
Heinrich Lukas Weil <hl.weil@t-online.de>
2023-11-22 15:08:36 +0100

    add querymodel test repo

---
+++ tests/ARCtrl.Querymodel.Tests/ArcTable.fs
@@ -0,0 +1,14 @@
+﻿module ArcTable.Tests
+
+open Expecto
+open System.Text.Json
+
+[<Tests>]
+let testStuff =
+
+    testList "ArcTable" [
+        testCase "WillNotFail" (fun () -> 
+            Expect.isTrue true "Test if the test will test."
+            
+        )
+    ]
