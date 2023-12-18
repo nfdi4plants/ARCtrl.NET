e020abe86dbfd9c22cd94f7959cd3e8e80146b4e
David Zimmer <davidzimmer91@gmail.com>
2023-12-18 22:23:15 +0100

    fix typos in sample arc, update playground.fsx

--- tests/ARCtrl.Querymodel.Tests/playground.fsx
+++ tests/ARCtrl.Querymodel.Tests/playground.fsx
@@ -9,7 +9,7 @@
 #r @"bin\Debug\net6.0\ARCtrl.ISA.Spreadsheet.dll"
 #r @"bin\Debug\net6.0\ARCtrl.Querymodel.dll"
 #r @"bin\Debug\net6.0\Fable.Core.dll"
-
+#r @"bin\Debug\net6.0\OBO.NET.dll"
 
 open ARCtrl
 open ARCtrl.NET
@@ -19,13 +19,69 @@ let testArcPath = __SOURCE_DIRECTORY__ + @"\TestObjects\TestArc"
 let testArc = ARC.load(testArcPath)
 
 let i = testArc.ISA.Value
+
 i.AssayCount
 i.StudyCount
+
 i.ArcTables.TableCount
 
+i.ArcTables.TableNames
 
-i.AssayIdentifiers
-let lastsamples = i.GetAssay("SamplePreparation_Cold").LastSamples
-lastsamples
+i.ArcTables.FirstRawData
 |> List.map (fun x -> x.Name)
+
+i.ArcTables.LastSamples
+|> List.map (fun x -> x.Name)
+
+i.ArcTables.FirstSamples
+|> List.map (fun x -> x.Name)
+
+i.ArcTables.LastRawData
+|> List.map (fun x -> x.Name)
+
+i.ArcTables.FirstProcessedData
+|> List.map (fun x -> x.Name)
+
+i.ArcTables.LastProcessedData
+|> List.map (fun x -> x.Name)
+
+i.ArcTables.Nodes
+|> List.map (fun x -> x.Name)
+
+let studies = i.StudyIdentifiers
+let heatAssays = i.Studies[0].RegisteredAssayIdentifiers |> List.ofSeq 
+
+let coldAssays = i.Studies[1].RegisteredAssayIdentifiers |> List.ofSeq
+
+let lastsamplesOfFirstAssayH = i.GetAssay("SamplePreparation_Heat").LastSamples |> List.map (fun x -> x.Name)
+let lastsamplesOfFirstAssayC = i.GetAssay("SamplePreparation_Cold").LastSamples |> List.map (fun x -> x.Name)
+
+let lastNodesHeat = i.GetAssay("MSEval_Heat").LastNodes |> Seq.map (fun x -> x.Name) |> Array.ofSeq
+let lastSamplesHeat = i.GetAssay("MSEval_Heat").LastSamples |> List.map (fun x -> x.Name) |> Array.ofSeq
+
+let exampleLastSampleHeat = i.GetAssay("MSEval_Heat").LastSamples[0] 
+let exampleLastSampleHeatName = i.GetAssay("MSEval_Heat").LastSamples[0].Name 
+
+let prevNodes = exampleLastSampleHeat.Sources |> List.ofSeq |> List.map (fun x -> x.Name)
+
+exampleLastSampleHeat.PreviousParameters
+|> Seq.map (fun x -> x.Value)
+|> List.ofSeq
+
+//  i.GetAssay("MSEval_Heat") |> List.map (fun x -> x.Name) |> Array.ofSeq
+let allValuesOfExampleAssayAndExampleNode = 
+    i.GetAssay("MSEval_Heat").ValuesOf("sampleOutHeat.txt")
+
+allValuesOfExampleAssayAndExampleNode
+|> Seq.map (fun x -> x.NameText)
+|> List.ofSeq
+
+exampleLastSampleHeat.Parameters
+|> Seq.toList
+|> List.map (fun x -> x.NameText)
+
+// let getBioRep (fN:QNode) = 
+//     match qi.ValuesOf(fN,ProtocolName = "Growth").WithName("biological replicate").Values.Head with
+//     | QueryModel.ISAValue.Parameter x -> x.Value.Value.AsString 
+//     | _ -> failwith "no biorep please add"
 
